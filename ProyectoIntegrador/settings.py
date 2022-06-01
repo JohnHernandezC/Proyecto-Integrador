fa2674b1a0844f9e7e0867e6fbee74a03d7e7100
Jhon Hernandez <johnher121099@gmail.com>
2022-05-31 22:02:32 -0500

    last last last commit

--- ProyectoIntegrador/settings.py
+++ ProyectoIntegrador/settings.py
@@ -26,9 +26,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-wh6)!gb17uyv3ul(9@pyegs3n#86df5k=xbc%=nfbl$cyka&7h'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
